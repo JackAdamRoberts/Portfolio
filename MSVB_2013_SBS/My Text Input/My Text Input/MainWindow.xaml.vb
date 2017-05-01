1504fab76bd6f97768d2bf702e4c19b27bcaea89
Jack Roberts <nllpitcher@gmail.com>
2017-04-30 21:45:16 -0400

    Added My Text Input Windows Store text input app.

---
+++ MSVB_2013_SBS/My Text Input/My Text Input/MainWindow.xaml.vb
@@ -0,0 +1,15 @@
+﻿Class MainWindow
+    Private Sub TestButton_Click(sender As Object, e As RoutedEventArgs) Handles TestButton.Click
+        Dim sampleText As String
+        sampleText = InputString.Text
+        If sampleText.Contains("River") Then
+            Output.Text = "The string 'River' was found."
+        Else
+            Output.Text = "The string 'River' was not found."
+        End If
+    End Sub
+
+    Private Sub InputString_TextChanged(sender As Object, e As TextChangedEventArgs) Handles InputString.TextChanged
+
+    End Sub
+End Class
