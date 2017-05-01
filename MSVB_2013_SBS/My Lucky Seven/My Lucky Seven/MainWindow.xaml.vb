fbfc823376e27f082ecbb8229c2630085d8ea2de
Jack Roberts <nllpitcher@gmail.com>
2017-04-30 21:09:51 -0400

    Added two projects completed in book.
    Projects completed as personal practice from Microsoft Visual Basic 2013 Step by Step book.

---
+++ MSVB_2013_SBS/My Lucky Seven/My Lucky Seven/MainWindow.xaml.vb
@@ -0,0 +1,15 @@
+﻿Class MainWindow
+    Private Sub SpinButton_Click(sender As Object, e As RoutedEventArgs) Handles SpinButton.Click
+        Dim generator As New Random
+        CoinImage.Visibility = Visibility.Collapsed
+
+        FirstNum.Text = generator.Next(0, 9)
+        SecondNum.Text = generator.Next(0, 9)
+        ThirdNum.Text = generator.Next(0, 9)
+
+        If (FirstNum.Text = "7") And (SecondNum.Text = "7") And (ThirdNum.Text = "7") Then
+            CoinImage.Visibility = Visibility.Visible
+            My.Computer.Audio.Play("C:\Users\jrobe\Desktop\ArcadeRiff.wav")
+        End If
+    End Sub
+End Class
